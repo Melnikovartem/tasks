be2f0d56e05a89a2508ea8228f5ac63fc1bb1659
Artem Melnikov <melnikovam10@gmail.com>
2017-09-20 00:55:27 +0300

    Everything works(no IO(). I will add later)

--- Haskell.hs
+++ Haskell.hs
@@ -5,35 +5,34 @@ result x = if length x == 1 then head $ head x else 'D'
 answer input = result $ filter line $ input ++ [[x!!num| x <- input]|num<-[0..2]] ++ [[input!!n!!n|n<-[0..2]]] ++ [[input!!n!!(2-n)|n<-[0..2]]]
 
 test = [
-    [[
+    ([
     "X.O",
     "XX.",
-    "XOO"], 'X'],
-    [["OO.",
+    "XOO"], 'X'),
+    (["OO.",
     "XOX",
-    "XOX"], 'O'],
-    [[
+    "XOX"], 'O'),
+    ([
     "OO.",
     "XOX",
-    "XOX"], 'O'],
-    [[
+    "XOX"], 'O'),
+    ([
     "XO.",
     "OX.",
-    "OOX"], "X"],
-
-    [[
+    "OOX"], 'X'),
+    ([
     "OOX",
     "OX.",
-    "XO."], "X"],
-    [[
+    "XO."], 'X'),
+    ([
     "OOX",
     "OXX",
-    "OXX"], "D"],
-    [[
+    "OXX"], 'D'),
+    ([
     "X.O",
     "O.X",
-    "O.X"], "D"]
+    "O.X"], '.')
     ]
 
-check x = if answer head x == answer !! 1 then True else False 
-main = foldr (&&) True $ map check test  	
+check x = (answer $ fst x) == (snd x)
+test_it = foldr (&&) True $ map check test  	
